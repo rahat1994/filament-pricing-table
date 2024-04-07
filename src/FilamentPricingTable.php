2c7522a73090c601ff684c5a78d5e2871e6b4205
Rahat Baksh <rahat392@gmail.com>
2024-04-08 02:26:34 +0600

    Refactor FilamentPricingTable class to implement Plugin interface

--- src/FilamentPricingTable.php
+++ src/FilamentPricingTable.php
@@ -2,6 +2,28 @@
 
 namespace RahatBaksh\FilamentPricingTable;
 
-class FilamentPricingTable
+use Filament\Contracts\Plugin;
+use Filament\Panel;
+
+class FilamentPricingTable implements Plugin
 {
+    public static function make(): static
+    {
+        return new self;
+    }
+
+    public function boot(Panel $panel): void
+    {
+        dd($panel);
+    }
+    // TODO: Implement register() method.
+
+    public function register(Panel $panel): void
+    {
+    }
+
+    public function getId(): string
+    {
+        return 'rahat-baksh-filament-pricing-table';
+    }
 }
